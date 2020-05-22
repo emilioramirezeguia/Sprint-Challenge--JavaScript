07467e3865446cfad8dba378884d5af250aa941b
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-05-22 10:56:09 -0500

    Complete step 3 on arrays and callbacks

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -103,9 +103,10 @@ console.log(multiply(5, 5));
 console.log(greeting("Emilio", "Ramirez"));
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log("==== Step 3: Check your work by un-commenting the following calls to consume() ====");
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
