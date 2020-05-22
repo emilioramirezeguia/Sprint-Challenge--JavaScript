b85395e85810df28cd187504c4e1eee50ca1bbe2
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-05-22 11:25:01 -0500

    Complete task 2 on closures

--- challenges/closure.js
+++ challenges/closure.js
@@ -24,3 +24,13 @@ myFunction();
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+console.log("=== Task 2: Counter ===");
+function sumation(number){
+  let total = 0;
+  for (let i = 0; i <= number; i++){
+    total += i;
+  }
+  return total;
+}
+console.log(sumation(4));
